566df50b60ed60726bfb4b0eab598a030af4b70b
NeeglesNorah <norah@neegles.com>
2025-10-13 17:18:28 +0100

    updates to heroo

--- src/components/heroo.tsx
+++ src/components/heroo.tsx
@@ -34,7 +34,7 @@ export const Hero3 = () => {
               className="px-4 py-2 sm:px-6 sm:py-3 text-black font-medium transition-colors duration-200 flex items-center justify-center gap-2 sm:gap-4 text-sm sm:text-base"
               style={{ backgroundColor: '#ffffff', cursor: 'pointer' }}
             >
-              Learn More 
+              Zoom Link
               <svg className="w-3 h-3 sm:w-4 sm:h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
@@ -44,7 +44,7 @@ export const Hero3 = () => {
               style={{ backgroundColor: '#ffffff', cursor: 'pointer' }}
               onClick={scrollToWaitlist}
             >
-              Register For Launch
+              Reserve your spot
               <svg className="w-3 h-3 sm:w-4 sm:h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.746 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
               </svg>
